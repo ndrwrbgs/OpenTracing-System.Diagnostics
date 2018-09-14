f9e54a3420d2bd51454d3a81fd4ab6c03c598323
ndrwrbgs <ndrwrbgs@users.noreply.github.com>
2018-09-14 13:02:52 -0700

    Fix #2

--- src/Library/ToOpenTracing/AsyncLocal.cs
+++ src/Library/ToOpenTracing/AsyncLocal.cs
@@ -1,27 +1,79 @@
-﻿namespace System.Threading
+﻿// ReSharper disable once CheckNamespace
+namespace System.Threading
 {
+#if NET45 // AsyncLocal is .NET 4.6+, so fall back to CallContext for .NET 4.5
     using System;
-#if NET45 // AsyncLocal is .NET 4.6+, so fall back to CallContext for .NET 4.5
-    using System.Runtime.Remoting;
     using System.Runtime.Remoting.Messaging;
-#endif
-
-#if NET45 // AsyncLocal is .NET 4.6+, so fall back to CallContext for .NET 4.5
-    internal sealed class AsyncLocal<T>
+    
+    /// <remarks>
+    /// Taken from https://github.com/StephenClearyArchive/AsyncLocal
+    /// Please see license at https://github.com/StephenClearyArchive/AsyncLocal/blob/master/LICENSE
+    /// </remarks>
+    /// <summary>
+    /// Data that is "local" to the current async method. This is the async near-equivalent of <c>ThreadLocal&lt;T&gt;</c>.
+    /// </summary>
+    /// <typeparam name="TImmutableType">The type of the data. This must be an immutable type.</typeparam>
+    internal sealed class AsyncLocal<TImmutableType>
     {
-        private static readonly string logicalDataKey = "__AsyncLocal_" + Guid.NewGuid();
+        /// <summary>
+        /// Our unique slot name.
+        /// </summary>
+        private readonly string _slotName = Guid.NewGuid().ToString("N");
 
-        public T Value
+        /// <summary>
+        /// The default value when none has been set.
+        /// </summary>
+        private readonly TImmutableType _defaultValue;
+
+        /// <summary>
+        /// Creates a new async-local variable with the default value of <typeparamref name="TImmutableType"/>.
+        /// </summary>
+        public AsyncLocal()
+            : this(default(TImmutableType))
+        {
+        }
+
+        /// <summary>
+        /// Creates a new async-local variable with the specified default value.
+        /// </summary>
+        public AsyncLocal(TImmutableType defaultValue)
+        {
+            _defaultValue = defaultValue;
+        }
+
+        /// <summary>
+        /// Returns a value indicating whether the value of this async-local variable has been set for the local context.
+        /// </summary>
+        public bool IsValueSet
+        {
+            get { return CallContext.LogicalGetData(_slotName) != null; }
+        }
+
+        /// <summary>
+        /// Gets or sets the value of this async-local variable for the local context.
+        /// </summary>
+        public TImmutableType Value
         {
             get
             {
-                var handle = CallContext.LogicalGetData(logicalDataKey) as ObjectHandle;
-                return (T) handle?.Unwrap();
+                var ret = CallContext.LogicalGetData(_slotName);
+                if (ret == null)
+                    return _defaultValue;
+                return (TImmutableType)ret;
             }
+
             set
             {
-                CallContext.LogicalSetData(logicalDataKey, new ObjectHandle(value));
+                CallContext.LogicalSetData(_slotName, value);
             }
+        }
+
+        /// <summary>
+        /// Clears the value of this async-local variable for the local context.
+        /// </summary>
+        public void ClearValue()
+        {
+            CallContext.FreeNamedDataSlot(_slotName);
         }
     }
 #endif
